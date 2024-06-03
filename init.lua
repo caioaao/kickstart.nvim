5942d89c1a990a978c9a461866b11b4eac20eaad
Caio Oliveira <caioaao@gmail.com>
2024-06-03 13:25:09 -0300

    remove oil.nvim
    cool, but buggy

--- init.lua
+++ init.lua
@@ -869,24 +869,6 @@ require('lazy').setup({
       }
     end,
   },
-  {
-    'stevearc/oil.nvim',
-    opts = {},
-    -- Optional dependencies
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    keys = {
-      {
-        '<leader>-',
-        function()
-          require('oil').open(nil)
-        end,
-        desc = 'Open parent dir',
-      },
-    },
-    config = function()
-      require('oil').setup()
-    end,
-  },
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
