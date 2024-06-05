27f9ed5cf310a9e9c8ca33d056aad86cf42692f8
Caio Oliveira <caioaao@gmail.com>
2024-06-05 16:18:46 -0300

    :Gpof

--- init.lua
+++ init.lua
@@ -231,6 +231,7 @@ require('lazy').setup({
     config = function()
       vim.api.nvim_create_user_command('Gco', 'G checkout -b <args> origin', { nargs = 1 })
       vim.api.nvim_create_user_command('Gpo', 'G push -u origin HEAD <args>', { nargs = '*' })
+      vim.api.nvim_create_user_command('Gpof', 'G push -u origin HEAD --force-with-lease', {})
       vim.api.nvim_create_user_command('Gro', 'G rebase origin/main', {})
       vim.api.nvim_create_user_command('Gri', 'G rebase --interactive origin/main', {})
       vim.api.nvim_create_user_command('Gra', 'G rebase --abort', {})
